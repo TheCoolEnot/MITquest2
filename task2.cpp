137228a2d33f6eff44b8a579d241832f2db534dc
TheCoolEnot <dmutruiprutkov203040@gmail.com>
2022-03-10 23:28:46 +0400

    New file with recursion function fib(n)

--- task2.cpp
+++ task2.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int n){
+    if (n < 3) return 1;
+    else return fib(n-1) + fib(n-2);
+}
+
 int main()
 {
-    std::cout << "Hello World";
-
+    int n;
+    cout << "Hello World" << endl;
+    cout << "Введите n для вычисление n-го числа Фиббоначчи. n = ";
+    cin >> n;
+    cout << fib(n);
     return 0;
 }
