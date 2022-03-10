1ba59779458e88b12677db6d516f4cbc67947a86
TheCoolEnot <dmutruiprutkov203040@gmail.com>
2022-03-10 23:43:21 +0400

    New file with iteration function fib(n)

--- task2.cpp
+++ task2.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 int fib(int n){
+    int x = 0, y = 1, z, r = 0;
     if (n == 1) return 0;
-    if (n == 2) return 1;
-    else return fib(n-1) + fib(n-2);
+    else if (n == 2) return 1;
+    else {
+        cout << 0 << endl << 1 << endl;
+        for (int i = 2; i < n; ++i){
+            z = x + y;
+            r = z;
+            cout << r << endl;
+            x = y;
+            y = z;
+        }
+        return r;
+    }
 }
 
 int main()
@@ -14,6 +25,6 @@ int main()
     cout << "Hello World" << endl;
     cout << "Введите n для вычисление n-го числа Фиббоначчи. n = ";
     cin >> n;
-    cout << fib(n);
+    fib(n);
     return 0;
 }
